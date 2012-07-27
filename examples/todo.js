ca3fbeb5036a46fc75d9e448175dc9a76b4fbcfd
CrypticSwarm <crypticswarm+git@gmail.com>
2012-07-27 17:00:45 -0500

    clean up code

--- examples/todo.js
+++ examples/todo.js
@@ -1,11 +1,8 @@
 var Task = QStrike.make('Task', { sync: { done: false, title: "" } }
-            , { close: QStrike.Closable })
-  , TaskList = QStrike.compose(Task
-            , 'TaskList'
-            , { subob: ['tasks'] }
-            , { addTaskList: QStrike.CreateType('TaskList', 'tasks')
-              , addTask: QStrike.CreateType('Task', 'tasks')
-              })
+              , { close: QStrike.Closable })
+var TaskList = QStrike.compose(Task, 'TaskList', { subob: ['tasks'] }
+              , { addTaskList: QStrike.CreateType('TaskList', 'tasks')
+                , addTask: QStrike.CreateType('Task', 'tasks') })
 
 QStrike.start({ appName: 'inventory-measure-727'
               , token: 'b7830f312bb64f2db14c349717fe3fa5'
